33b2d0ee3b95ada1f8e7917e9ab03d4e949a63c6
Carl Beaverson <carlbeaverson@Carls-MacBook-Pro.local>
2025-12-22 11:29:28 +1000

    Change to hero title

--- src/components/journalHero.js
+++ src/components/journalHero.js
@@ -5,7 +5,7 @@ export default function JournalHero({ title, category, asset }) {
     return (
         <div className='p10'>
             {category && <p className='gerstner fw-300 op-50 text-center mb10'>{category}</p>}
-            <h1 className='h1 text-center mb-20'>{title}</h1>
+            <h1 className='h0 large text-center mb-20'>{title}</h1>
             <div className='ratio-16-10 bg-grey pos-rel overflow'>
                 <MediaAsset video={asset?.video} image={asset?.image} />
             </div>
